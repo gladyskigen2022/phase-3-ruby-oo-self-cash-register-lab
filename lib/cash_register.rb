75f44dd427b24bad19ef09d575ed8912d883f3f2
GladysKigen <gladys.kigen@student.moringa.com>
2023-02-13 20:48:52 +0300

    initialization

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+#.new
+   # takes one optional argument, an employee discount, on initialization (FAILED - 1)
+   # sets an instance variable @total on initialization to zero (FAILED - 2)
+
+class CashRegister
+    attr_accessor :total
+    attr_reader :discount
+
+    def initialize(discount)
+        @discount = discount
+        @total = 0
+    end
+end
+
+    #def add_item(title, price, quantity)
+
 
