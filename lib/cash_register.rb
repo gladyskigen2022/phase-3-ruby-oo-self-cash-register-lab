49f99fbdbc414674b3d8e58f1f9140f9dcf22f05
GladysKigen <gladys.kigen@student.moringa.com>
2023-02-13 21:28:36 +0300

    discount

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@
    # sets an instance variable @total on initialization to zero (FAILED - 2)
 
 class CashRegister
-    attr_accessor :total, :discount, :items, last_transaction
+    attr_accessor :total, :discount, :items, :last_transaction
     
 
     def initialize(discount=0)
@@ -14,12 +14,24 @@ class CashRegister
 
    def add_item(title, price, quantity=1) 
     self.total += price * quantity
-    quantity.times do 
+        quantity.times do 
         #<< shovel pushes an object into an array
         items << title
+        end
+    self.last_transaction = price * quantity
     end
 
-    
+   def apply_discount
+     if discount !=0
+        self.total = (total * ((100.0 - discount.to_f)/100)).t0_i
+        "After the discount, the total comes to $#{self.total}."
+     else
+        "There is no discount to apply."
+     end
+   end
+
+
+
 
 
 
